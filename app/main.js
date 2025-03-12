29683cda49f369bf81777eb06dbb4c70aa30b5b7
Redminer9630 <robertchalupka5@gmail.com>
2025-03-12 09:19:50 +0100

    Create main.js

---
+++ app/main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+app.whenReady().then(() => {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    mainWindow.loadFile('src/index.html');
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
